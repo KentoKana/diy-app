d4cf2ebc5f1c3751e463ba4082c6c39ec224d88e
Kento Kanazawa <33736354+KentoKana@users.noreply.github.com>
2020-05-24 18:55:50 -0400

    Styled login page

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -3,6 +3,7 @@
   import jwtdecode from "jwt-decode";
   import { isLoggedIn } from "../../stores/store.js";
   import Textfield from "@smui/textfield";
+  import Button, { Label } from "@smui/button";
   let email = "";
   let password = "";
   let inputValues = {
@@ -51,7 +52,42 @@
 </script>
 
 <style lang="scss">
-
+  @import "../../style/theme.scss";
+  .login {
+    h1 {
+      text-align: center;
+    }
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: calc(100vh - 57px);
+    .form__wrap {
+      display: flex;
+      flex-direction: column;
+      width: 400px;
+      height: 500px;
+      // background-color: grey;
+      align-items: center;
+      justify-content: center;
+      border-radius: $border-radius-lg;
+      box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.2);
+    }
+    .form {
+      width: 201px;
+      height: 300px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      margin: 0 auto;
+      .submit-button {
+        display: flex;
+        justify-content: flex-end;
+        margin: 10px 0;
+      }
+    }
+  }
 </style>
 
 <svelte:head>
@@ -60,32 +96,42 @@
     href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 </svelte:head>
 <section class="login">
-  <form on:submit|preventDefault={handleSubmit}>
-    <div>
-      <label for="email" class="email">E-mail</label>
-      <Textfield
-        type="text"
-        placeholder="Please type your username"
-        name="email"
-        bind:value={inputValues.email}
-        id="email" />
-      <div class="validation-message">
-        {#if validationMessages.email !== ''}{validationMessages.email}{/if}
+  <div class="form__wrap">
+    <form on:submit|preventDefault={handleSubmit} class="form">
+      <h1>Login to <span class="primary-highlight">DooIt</span></h1>
+      <div>
+        <Textfield
+          type="text"
+          placeholder="Please type your username"
+          name="email"
+          label="E-mail"
+          bind:value={inputValues.email}
+          id="email" />
+        <div class="validation-message">
+          {#if validationMessages.email !== ''}{validationMessages.email}{/if}
+        </div>
       </div>
-    </div>
-    <div>
-      <label for="password" class="password">Password</label>
-      <input
-        type="password"
-        placeholder="Please type your password"
-        name="password"
-        bind:value={inputValues.password} />
-      <div class="validation-message">
-        {#if validationMessages.password !== ''}
-          {validationMessages.password}
-        {/if}
+      <div>
+        <Textfield
+          type="password"
+          placeholder="Please type your password"
+          name="password"
+          label="Password"
+          bind:value={inputValues.password} />
+        <div class="validation-message">
+          {#if validationMessages.password !== ''}
+            {validationMessages.password}
+          {/if}
+        </div>
+        <div class="submit-button">
+          <Button variant="unelevated" type="submit">Login</Button>
+        </div>
       </div>
-      <button type="submit">Login</button>
-    </div>
-  </form>
+    </form>
+    <p>
+      Don't have an account? Register
+      <a href="@">here</a>
+      !
+    </p>
+  </div>
 </section>
